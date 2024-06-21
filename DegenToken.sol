84edb9cae2e534d8f1f0544ea08445327f7b9d8b
siddharth-nain <124575984+siddharth-nain@users.noreply.github.com>
2024-06-21 17:45:25 +0530

    Update DegenToken.sol

--- DegenToken.sol
+++ DegenToken.sol
@@ -5,53 +5,102 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DengenToken is ERC20, Ownable {
-   
     constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {}
 
-    
-    event TokensRedeemed(address indexed redeemer, uint256 amount, string item);
-
-   
-    mapping(string => uint256) private itemCosts;
-
-    
-    function setItemCost(string memory item, uint256 cost) public onlyOwner {
-        require(bytes(item).length > 0, "Item name cannot be empty");
-        require(cost > 0, "Item cost must be greater than zero");
-        itemCosts[item] = cost;
+    struct RedeemedItem {
+        uint256 amount;          
+        uint256 tokensRedeemed;  
     }
+
+    event TokensRedeemed(address indexed redeemer, uint256 amount, uint256 tokensRedeemed);
+
+    uint256 public tokenCost = 1; 
+
+    mapping(address => RedeemedItem[]) private redeemedItems;
+
     
-     function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) public onlyOwner {
         require(to != address(0), "Cannot mint to zero address");
         require(amount > 0, "Mint amount must be greater than zero");
         _mint(to, amount);
     }
 
-       function burn(uint256 amount) public {
+    
+    function burn(uint256 amount) public {
         require(amount > 0, "Burn amount must be greater than zero");
         _burn(_msgSender(), amount);
     }
 
-
-        function redeem(string memory item) public {
-        uint256 cost = itemCosts[item];
-        require(cost > 0, "Item does not exist or cost not set");
+   
+    function redeem(uint256 amount) public {
+        require(amount > 0, "Redeem amount must be greater than zero");
+        uint256 cost = amount * tokenCost;
         require(balanceOf(_msgSender()) >= cost, "Insufficient token balance");
 
-
+       
         _burn(_msgSender(), cost);
 
-  
-        emit TokensRedeemed(_msgSender(), cost, item);
+       
+        redeemedItems[_msgSender()].push(RedeemedItem({
+            amount: amount,
+            tokensRedeemed: cost
+        }));
+
+        emit TokensRedeemed(_msgSender(), amount, cost);
     }
 
+    
+    function getRedeemedItems(address account) public view returns (RedeemedItem[] memory) {
+        require(account != address(0), "Query for zero address");
+        return redeemedItems[account];
+    }
 
+    
+    function printRedeemedTokens(address account) public view returns (string memory) {
+        require(account != address(0), "Query for zero address");
+        RedeemedItem[] memory items = redeemedItems[account];
+        require(items.length > 0, "No redeemed tokens found");
+
+        string memory result = "";
+        for (uint i = 0; i < items.length; i++) {
+            result = string(abi.encodePacked(
+                result,
+                "Redemption ", uintToString(i + 1), ": ", 
+                "Amount: ", uintToString(items[i].amount), 
+                " Tokens Redeemed: ", uintToString(items[i].tokensRedeemed), 
+                "\n"
+            ));
+        }
+        return result;
+    }
+
+    
+    function uintToString(uint256 v) internal pure returns (string memory) {
+        if (v == 0) {
+            return "0";
+        }
+        uint256 digits;
+        uint256 temp = v;
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+        bytes memory buffer = new bytes(digits);
+        while (v != 0) {
+            digits -= 1;
+            buffer[digits] = bytes1(uint8(48 + uint256(v % 10)));
+            v /= 10;
+        }
+        return string(buffer);
+    }
+
+   
     function checkBalance(address account) public view returns (uint256) {
         require(account != address(0), "Query for zero address");
         return balanceOf(account);
     }
 
-
+    
     function transferTokens(address from, address to, uint256 amount) public {
         require(amount > 0, "Transfer amount must be greater than zero");
         require(from != address(0), "Cannot transfer from zero address");
